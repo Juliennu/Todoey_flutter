51af21eeea3ee69ae2c6979c4c51d588ca8562cf
Juliennu <wsb.1099@gmail.com>
2023-08-22 10:59:18 +0900

    ListTitleを用いてリファクタリング

--- lib/widgets/task_row.dart
+++ lib/widgets/task_row.dart
@@ -16,29 +16,26 @@ class TaskRow extends StatefulWidget {
 class _TaskRowState extends State<TaskRow> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          widget.task.title,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            // 取り消し線
-            decoration: widget.task.isChecked
-                ? TextDecoration.lineThrough
-                : TextDecoration.none,
-            decorationThickness: 2,
-          ),
+    return ListTile(
+      title: Text(
+        widget.task.title,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          // 取り消し線
+          decoration: widget.task.isChecked
+              ? TextDecoration.lineThrough
+              : TextDecoration.none,
+          decorationThickness: 2,
         ),
-        Checkbox(
-            value: widget.task.isChecked,
-            onChanged: (value) {
-              setState(() {
-                widget.task.isChecked = !widget.task.isChecked;
-              });
-            }),
-      ],
+      ),
+      trailing: Checkbox(
+          value: widget.task.isChecked,
+          onChanged: (value) {
+            setState(() {
+              widget.task.isChecked = !widget.task.isChecked;
+            });
+          }),
     );
   }
 }
